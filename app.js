18aa70d4f8b91f7452969dbf0b316debad504232
NicoExcalibur <nico.zanz@gmail.com>
2021-05-26 17:26:53 +0200

    edit function to change values

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
-  console.log(this.value);
+  const suffix = this.dataset.sizing || ""; // select the 'data-...' or nothing
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
